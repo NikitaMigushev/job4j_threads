cfce374849c6480055023fad818a810b9a0fd75f
Nikita Migushev <migushev@gmail.com>
2023-06-04 11:38:14 +0300

    4. Режим ожидания. [#231217 # [#231217 #420639]]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -4,10 +4,10 @@ public class Wget {
     public static void main(String[] args) {
         Thread thread = new Thread(
                 () -> {
-                    for (int i = 0; i < 100; i++) {
+                    for (int i = 0; i <= 100; i++) {
                         System.out.println("\rLoading: " + i + "%");
                         try {
-                            Thread.sleep(1000);
+                            Thread.sleep(100);
                         } catch (InterruptedException e) {
                             Thread.currentThread().interrupt();
                         }
