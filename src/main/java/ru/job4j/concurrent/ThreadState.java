2dc9928ea7d4d975a8c8e63aad00b29995d69e7d
Nikita Migushev <migushev@gmail.com>
2023-06-04 11:58:23 +0300

    3. Состояние нити. [#229175 # [#229175 #420592]]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -14,8 +14,8 @@ public class ThreadState {
         );
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED ||
-                second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
             System.out.println("First thread state is: " + first.getState());
             System.out.println("Second thread state is: " + second.getState());
         }
