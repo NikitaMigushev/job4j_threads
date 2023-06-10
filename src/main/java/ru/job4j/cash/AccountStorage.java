8894b34ddcaaddb890c02e239261547d519fd516
Nikita Migushev <migushev@gmail.com>
2023-06-10 14:54:44 +0300

    3. Денежные переводы AccountStorage [#1104 # [#1104 #422083]]

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -10,47 +10,25 @@ public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
     synchronized public boolean add(Account account) {
-        boolean result = false;
-        if (accounts.get(account.id()) == null) {
-            accounts.put(account.id(), account);
-            result = true;
-        }
-        return result;
+       return accounts.putIfAbsent(account.id(), account) == null;
     }
 
     synchronized public boolean update(Account account) {
-        boolean result = false;
-        Optional searchAccount = getById(account.id());
-        if (searchAccount.isPresent()) {
-            accounts.put(account.id(), account);
-            result = true;
-        }
-        return result;
+        return accounts.replace(account.id(), account) != null;
     }
 
     synchronized public boolean delete(int id) {
-        boolean result = false;
-        Optional searchAccount = getById(id);
-        if (searchAccount.isPresent()) {
-            accounts.remove(id);
-            result = true;
-        }
-        return result;
+        return accounts.remove(id) != null;
     }
 
     synchronized public Optional<Account> getById(int id) {
-        Optional result = Optional.empty();
-        Account account = accounts.get(id);
-        if (account != null) {
-            result = Optional.of(account);
-        }
-        return result;
+        return Optional.ofNullable(accounts.get(id));
     }
     public boolean transfer(int fromId, int toId, int amount) {
         boolean result = false;
         synchronized (accounts) {
-            Account fromAccount = accounts.get(fromId);
-            Account toAccount = accounts.get(toId);
+            Account fromAccount = getById(fromId).get();
+            Account toAccount = getById(toId).get();
             if (fromAccount != null && toAccount != null
                     && fromAccount.amount() >= amount) {
                 Account updatedFromAccount = new Account(fromAccount.id(), fromAccount.amount() - amount);
