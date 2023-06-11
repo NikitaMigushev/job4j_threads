b4c18977290587f44d315709f6586d8c2339d186
Nikita Migushev <migushev@gmail.com>
2023-06-11 17:32:35 +0300

    3. Junit тест для блокирующей очереди. [#68589 # [#68589]]

--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
@@ -78,5 +79,42 @@ class SimpleBlockingQueueTest {
         consumer.interrupt();
         consumer.join();
         assertThat(buffer).contains(0, 1, 2, 3, 4);
+    }
+
+    @Test
+    public void whenFetchAllThenGetItString() throws InterruptedException {
+        final CopyOnWriteArrayList<String> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<String> queue = new SimpleBlockingQueue<>(10);
+        List<String> strings = List.of("A", "B", "C");
+        Thread producer = new Thread(
+                () -> {
+                    for (String string : strings) {
+                        try {
+                            queue.offer(string);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).contains("A", "B", "C");
     }
 }
